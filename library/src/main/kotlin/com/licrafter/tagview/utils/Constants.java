d9bd3d95d9eae0c5a8910a342279728fc4f2247d
chenqiuxi <1967919189@qq.com>
2019-11-22 18:25:30 +0800

    小红书标签

---
+++ library/src/main/kotlin/com/licrafter/tagview/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.licrafter.tagview.utils;
+
+/**
+ * Create on 2019/11/22 18:04
+ * author revstar
+ * Email 1967919189@qq.com
+ */
+public class Constants {
+    public static final int SPACE=50;
+}
